25a93cbac7ce14a1cb7e75dc1c7f03ac9a6397c3
Isabelle George and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-04-20 16:09:53 -0700

    commit before adding functions

--- app/src/main/java/com/example/guest/boggle/AnswerActivity.java
+++ app/src/main/java/com/example/guest/boggle/AnswerActivity.java
@@ -3,6 +3,7 @@ package com.example.guest.boggle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,6 +15,9 @@ import butterknife.ButterKnife;
 public class AnswerActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     @Bind(R.id.answerListView) ListView mAnswerListView;
+    private static ArrayList<String> mGeneratedCharArray;
+    private static ArrayList<String> mAnswers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +25,27 @@ public class AnswerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_answer);
         ButterKnife.bind(this);
 
+
+
+
         Intent intent = getIntent();
+
         ArrayList<String> answers = intent.getStringArrayListExtra("answers");
 
+        ArrayList<String> generatedCharArray = intent.getStringArrayListExtra("generatedCharArray");
+        mGeneratedCharArray = generatedCharArray;
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, answers);
+        mAnswers = answers;
         mAnswerListView.setAdapter(adapter);
 
 
     }
+
+
+//    public boolean wordChecker(){
+//
+//        }
+
 }
+
+
--- app/src/main/java/com/example/guest/boggle/MainActivity.java
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -67,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AnswerActivity.class);
+                intent.putStringArrayListExtra("generatedCharArray", generatedChar);
                 intent.putStringArrayListExtra("answers", answers);
                 startActivity(intent);
             }
