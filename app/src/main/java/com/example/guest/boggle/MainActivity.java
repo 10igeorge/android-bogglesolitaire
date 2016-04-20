39d5f1e3f0412e530a6205e3e506d33302c222ff
Isabelle George and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-04-20 10:09:23 -0700

    randomly generate array of characters including two vowels

--- app/src/main/java/com/example/guest/boggle/MainActivity.java
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.guest.boggle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private String[] vowels = new String[] {"a", "a", "a", "e", "e", "e", "i", "i", "i", "o", "o", "o", "u", "u","u",};
+    private String[] consonants = new String[] {"b", "b", "c", "d", "d", "f", "f", "g", "g", "h", "h", "j", "k", "l", "l", "m", "m", "p", "p", "qu", "r", "r", "s", "s", "t", "t", "v", "w", "x", "y", "z"};
+    private ArrayList<String> generatedChar = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 6; i++){
+            int rando = new Random().nextInt(consonants.length);
+            generatedChar.add(consonants[rando]);
+            Log.v("consonant", consonants[rando]);
+        }
+        for (int i = 0; i < 2; i++){
+            int rando = new Random().nextInt(vowels.length);
+            generatedChar.add(vowels[rando]);
+        }
     }
 }
